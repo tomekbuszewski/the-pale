8115783723e95595a9df187b334bf2d895ff43b8
Tomasz Buszewski <tomek.buszewski@gmail.com>
2025-02-06 13:53:12 +0100

    fix: client icons

--- app/ui/organisms/ClientsSection/helpers/ClientIcon.tsx
+++ app/ui/organisms/ClientsSection/helpers/ClientIcon.tsx
@@ -1,7 +1,10 @@
 import clsx from "clsx";
+import { motion } from "motion/react";
 
 import type { Client } from "@common-types/Client";
 import type { CSSProperties } from "react";
+
+import { itemVariants } from "./random";
 
 import styles from "../ClientsSection.module.scss";
 
@@ -12,7 +15,7 @@ export function ClientIcon({
   count,
 }: Client & { count: number }) {
   return (
-    <figure
+    <motion.figure
       key={name}
       className={styles.item}
       data-count={count + 1}
@@ -20,11 +23,16 @@ export function ClientIcon({
         ["--color" as keyof CSSProperties]: color,
         ["--shadow" as keyof CSSProperties]: `${color.replace(")", "")}-shadow)`,
       }}
+      initial="initial"
+      animate="animate"
+      exit="exit"
+      transition={{ duration: 0.4, delay: 0.1 * count }}
+      variants={itemVariants}
     >
       <Component />
       <figcaption className={clsx(styles.itemName, "sr-only")}>
         Logotype of my client {name}
       </figcaption>
-    </figure>
+    </motion.figure>
   );
 }
