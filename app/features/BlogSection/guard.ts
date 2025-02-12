1deaa70b94e6999c2fbb033e1c29732a193d6dbd
Tomasz Buszewski <tomek.buszewski@gmail.com>
2025-02-12 12:12:56 +0100

    fix: blog section missing

--- app/features/BlogSection/guard.ts
+++ app/features/BlogSection/guard.ts
@@ -4,10 +4,13 @@ import type { BlogPost } from "@common-types/Blogpost";
 export default function (
   input: unknown,
 ): input is { items: BlogPost[]; pagination: PaginationProps } {
+  console.log("INPUT", input);
   if (typeof input === "object" && input !== null) {
     const hasItems = "items" in input && (input.items as BlogPost[]).length > 0;
     const hasPosts = hasItems && "link" in (input.items as BlogPost[])[0];
     const hasPagination = "pagination" in input;
+
+    console.log({ hasPagination, hasPosts, hasItems });
 
     return hasItems && hasPosts && hasPagination;
   }
