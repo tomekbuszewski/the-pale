d9b18549795ef78bf495e3c8ac08309c5f58bcbe
Tomasz Buszewski <tomek.buszewski@gmail.com>
2025-02-13 00:37:03 +0100

    feat: contact button loads after mount

--- app/ui/atoms/ContactButton/ContactButton.tsx
+++ app/ui/atoms/ContactButton/ContactButton.tsx
@@ -1,3 +1,4 @@
+import { useLayoutEffect, useState } from "react";
 import { Link } from "react-router";
 import { Contact } from "@nav";
 import clsx from "clsx";
@@ -8,13 +9,26 @@ import buttonStyles from "../Button/Button.module.scss";
 import styles from "./ContactButton.module.scss";
 
 function ContactButton() {
+  const [loaded, setLoaded] = useState(false);
+
+  useLayoutEffect(() => {
+    setLoaded(true);
+  }, []);
+
   return (
     <Link
       to={Contact.href}
-      className={clsx(styles.wrapper, buttonStyles.base, buttonStyles.tertiary)}
+      className={clsx(
+        styles.wrapper,
+        buttonStyles.base,
+        buttonStyles.tertiary,
+        {
+          [styles.hidden]: !loaded,
+        },
+      )}
     >
       <div className={styles.imgWrapper}>
-        <img src={tomek} alt="Book a call!" />
+        <img src={loaded ? tomek : ""} alt="Book a call!" />
       </div>
       Contact me
     </Link>
