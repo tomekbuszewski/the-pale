47b4f222293d09b46c4732866c03c400cb25a027
Tomasz Buszewski <tomek.buszewski@gmail.com>
2025-02-25 16:21:17 +0100

    feat: pages moved to accommodate two languages

--- app/features/BlogSection/BlogSection.tsx
+++ app/features/BlogSection/BlogSection.tsx
@@ -3,6 +3,7 @@ import { BlogSection as Main } from "@ui/organisms";
 import type { PaginationProps } from "@common-types/BlogPagination";
 import type { BlogPost } from "@common-types/Blogpost";
 import { Content } from "@features";
+import { useContext } from "react";
 
 export default function BlogSection({
   items,
@@ -12,6 +13,9 @@ export default function BlogSection({
   pagination: PaginationProps;
 }) {
   const translate = Content.hooks.useTranslate();
+  const currentLanguage = useContext(Content.context);
+
+  if (currentLanguage !== "en") return null;
 
   return (
     <Main
--- app/routes.ts
+++ app/routes.ts
@@ -10,16 +10,23 @@ import { Routes } from "./nav";
 
 export default [
   layout("./layouts/Main.tsx", [
-    index("pages/home.tsx"),
-    route(Routes.website, "pages/services/web-development/index.tsx"),
-    route(Routes.frontend, "pages/services/front-end/index.tsx"),
-    route(Routes.consulting, "pages/services/consulting/index.tsx"),
-    route(Routes.uses, "pages/uses.tsx"),
-    route(Routes.tech, "pages/tech.tsx"),
-    // route("/services", "routes/services/index.tsx"),
+    index("pages/en/home.tsx"),
+    route(Routes.website, "pages/en/services/web-development/index.tsx"),
+    route(Routes.frontend, "pages/en/services/front-end/index.tsx"),
+    route(Routes.consulting, "pages/en/services/consulting/index.tsx"),
+    route(Routes.uses, "pages/en/uses.tsx"),
+    route(Routes.tech, "pages/en/tech.tsx"),
+
     route(Routes.post, "pages/blog/post.tsx"),
     route(Routes.pagination, "pages/blog/pagination.tsx"),
 
-    ...prefix("/pl", [index("pages/pl/home.tsx")]),
+    ...prefix("/pl", [
+      index("pages/pl/home.tsx"),
+      route(Routes.website, "pages/pl/services/web-development/index.tsx"),
+      route(Routes.frontend, "pages/pl/services/front-end/index.tsx"),
+      route(Routes.consulting, "pages/pl/services/consulting/index.tsx"),
+      route(Routes.uses, "pages/pl/uses.tsx"),
+      route(Routes.tech, "pages/pl/tech.tsx"),
+    ]),
   ]),
 ] satisfies RouteConfig;
